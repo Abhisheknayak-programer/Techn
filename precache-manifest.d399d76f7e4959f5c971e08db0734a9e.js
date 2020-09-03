cc09d2b33db743702b63f0e2d2a39874f042dece
Abhishek nayak <sahilservice200@gmail.com>
2020-09-03 08:39:01 +0530

    Updates

---
+++ precache-manifest.d399d76f7e4959f5c971e08db0734a9e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b57a820252661b95602b11aa7dbd503",
+    "url": "/Techn/index.html"
+  },
+  {
+    "revision": "9a365d1696bec9aa2af2",
+    "url": "/Techn/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "2aa03f07450a56bcec55",
+    "url": "/Techn/static/css/main.5c2b4c6f.chunk.css"
+  },
+  {
+    "revision": "9a365d1696bec9aa2af2",
+    "url": "/Techn/static/js/2.4725363e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Techn/static/js/2.4725363e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa03f07450a56bcec55",
+    "url": "/Techn/static/js/main.87341f6c.chunk.js"
+  },
+  {
+    "revision": "f2816230315708c8b24d",
+    "url": "/Techn/static/js/runtime-main.da59d441.js"
+  },
+  {
+    "revision": "0b905b27a06eaf4d477bfb163889f6c4",
+    "url": "/Techn/static/media/cartoon 3.0b905b27.svg"
+  },
+  {
+    "revision": "64fb8a6c031e021a72de8ea970588908",
+    "url": "/Techn/static/media/contact.64fb8a6c.svg"
+  },
+  {
+    "revision": "3b78283fcdbcf93228cae93937281ae9",
+    "url": "/Techn/static/media/drone.3b78283f.jpg"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/Techn/static/media/error.6695af99.svg"
+  },
+  {
+    "revision": "fa2736864110935bc2d3a99087882864",
+    "url": "/Techn/static/media/img1.fa273686.jpg"
+  },
+  {
+    "revision": "d043e42ecbf27a0737a6e341d33f2316",
+    "url": "/Techn/static/media/launcher.d043e42e.svg"
+  },
+  {
+    "revision": "c2cff6e904904a62e60300887650c310",
+    "url": "/Techn/static/media/ml.c2cff6e9.png"
+  },
+  {
+    "revision": "a0b7ce22e01a8578f602fb5a818eb48d",
+    "url": "/Techn/static/media/robort.a0b7ce22.jpg"
+  },
+  {
+    "revision": "f00e42f69c8850db992abcc19f14c0fa",
+    "url": "/Techn/static/media/security.f00e42f6.svg"
+  },
+  {
+    "revision": "2f7380b697331138e4cecf84d42535b7",
+    "url": "/Techn/static/media/service1.2f7380b6.png"
+  },
+  {
+    "revision": "67615083e94f5386da655cdb9d36ce4a",
+    "url": "/Techn/static/media/service2.67615083.png"
+  },
+  {
+    "revision": "c2f5afcf713bf3bac5ff79a1943ed3b7",
+    "url": "/Techn/static/media/service3.c2f5afcf.png"
+  },
+  {
+    "revision": "cd99f224f18fa02f245124b9653d50ee",
+    "url": "/Techn/static/media/service5.cd99f224.png"
+  },
+  {
+    "revision": "9612499c829a6f197e00c4adff5e99d0",
+    "url": "/Techn/static/media/service6.9612499c.png"
+  },
+  {
+    "revision": "b7aa7d85a3dd90b1246a8928637ecca9",
+    "url": "/Techn/static/media/service7.b7aa7d85.png"
+  },
+  {
+    "revision": "a2fb9808c152cc2def8a69ffcfa9f538",
+    "url": "/Techn/static/media/service9.a2fb9808.png"
+  },
+  {
+    "revision": "e7029bb4d9186db50458d73dcf056547",
+    "url": "/Techn/static/media/world.e7029bb4.jpg"
+  }
+]);
